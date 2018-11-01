a926458365a7499504c04601a43a799d36d9c36d
cane <canelp94@gmail.com>
2018-11-01 13:51:33 +0100

    rsm_ef commented, formatted input/output

--- 03_RSM_EF/rsm_ef.py
+++ 03_RSM_EF/rsm_ef.py
@@ -1,3 +1,4 @@
+# Revised Simplex Method with Eta factorization
 import numpy as np
 import sys
 
@@ -35,12 +36,21 @@ class RSM_EF:
     def solve_problem(self):
         x0 = np.array(np.zeros(shape=(1, self.initial_var_num)))
         x0 = np.concatenate((x0, self.b), axis=None)
+        # Check if the initial solution is feasible
+        # Ukoliko se u pocetnom bazisnom resenju x0 nalazi broj, koji je manji od nule, nije ispunjen uslov
+        # pozitivnosti resenja, pa vracamo odgovarajucu poruku i izlazimo iz algoritma.
+        if np.count_nonzero(x0 < 0) > 0:
+            print("System can't be solved using Revised Simplex Method with Eta .")
+            return
+        # Odredjujemo inicijalne P i Q
         p = [i for i in range(self.initial_var_num, self.a.shape[1])]
         q = [i for i in range(self.initial_var_num)]
         self.c = np.c_[self.c, np.zeros(shape=(1, self.a.shape[1]-self.initial_var_num))]
         B = self.a[:, p]
         E = np.eye(len(p))
         eta_matrices = []
+
+        # Stampamo i input radi boljeg formatiranja output fajla
         print("Input:\nA = \n%s,\n b = %s," % (self.a[:, :-self.initial_var_num], self.b))
         if self.f_opt_sign == -1:
             print("(max) c = %s" % self.c[0])
@@ -55,18 +65,23 @@ class RSM_EF:
         while True:
             cb = np.array(self.c[0, p])
             cn = np.array(self.c[0, q])
+            # Izracunavanje nove matrice B
             B = np.matmul(B, E)
             v = []
+            # Odredjivanje vektora v, w, ... iterativno, korisnjem informacija o prethodnim eta matricama
             for i in range(len(eta_matrices) - 1):
                 if i == 0:
                     v.append(np.matmul(cb, np.linalg.inv(eta_matrices[-1])))
                 else:
                     v.append(np.matmul(v[-i], np.linalg.inv(eta_matrices[-1-i])))
+            # Odredjivanje vektora u
             u = np.matmul(cb, np.linalg.inv(B))
 
             N = self.a[:, q]
             cn_p = cn - np.matmul(u, N)
 
+            # Optimal solution is found if there are no negative rj values
+            # Ukoliko su svi elementi rj > 0, optimalno resenje je pronadjeno
             if np.count_nonzero(cn_p < 0) == 0:
                 print("*********************************")
                 print("Optimal solution is found after %d iterations:\nx0: %s" % (iter_num, str(x0)))
@@ -76,11 +91,13 @@ class RSM_EF:
                 print("f_opt = %s" % (u.dot(self.b)*self.f_opt_sign))
                 break
 
+            # Pronalazimo odgovarajuce rj < 0
             j = 0
             for j in range(len(cn_p)):
                 if cn_p[j] < 0:
                     break
 
+            # Odredjujemo y, pa pravimo x(t) - cuvamo u vektoru t
             y = np.zeros(shape=(1, self.a.shape[1]))
             y[:, p] = np.linalg.solve(B, self.a[:, q[j]])
             t = np.zeros(shape=(1, self.a.shape[1]))[0]
@@ -93,10 +110,12 @@ class RSM_EF:
                 elif elem in q:
                     t[elem] = 0
 
+            # Ukoliko su svi yp, p pripada P, nepozitivni, funkcija cilja neograniceno opada ka -inf
             if np.count_nonzero(y[0] > 0) == 0:
                 print("Problem is unlimited, f->-inf.")
                 return
 
+            # Odredjujemo optimalno t, t kapa
             t_mins = []
             for i in range(len(t)):
                 if i in p and y[0][i] > 0 and x0[i]*y[0][i] > 0:
@@ -105,6 +124,7 @@ class RSM_EF:
 
             prod = t_min*y
 
+            # Biramo l takvo da je xl - tkapa*yl > 0
             l = 0
             for l in range(len(x0)):
                 if x0[l] != 0:
@@ -118,9 +138,12 @@ class RSM_EF:
             print("Current P is: %s" % str(p))
             print("Current Q is: %s" % str(q))
             print("f = %s" % (u.dot(self.b)*self.f_opt_sign))
+            # Kreiranje nove eta matrice, koja ce se koristiti u narednoj iteraciji i smestanje trenutne u niz
+            # eta matrica, koji cemo koristiti za iterativno racunanje vektora v, w, ...
             E = np.eye(len(p))
             E[:, p.index(l)] = y[0, p]
             eta_matrices.append(E)
+            # Azuriramo skupove P i Q, koji sadrze bazisne, odnosno nebazisne kolone
             p[p.index(l)] = q[j]
             q.remove(q[j])
             q.append(l)
