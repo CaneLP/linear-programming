1411706d6e5417a945593fdb1b2efed307fe3f6e
cane <canelp94@gmail.com>
2018-11-14 23:27:01 +0100

    TP done, cycle finder fixed (with hardcoded testcases)

--- 06_TransportationProblem/tp.py
+++ 06_TransportationProblem/tp.py
@@ -33,9 +33,18 @@ class TP:
         self.m = num_storages
         self.n = num_shops
         self.a = a
+        self.initial_a = copy.deepcopy(a)
         self.b = b
         self.c = matrix_c
+        self.initial_c = copy.deepcopy(matrix_c)
         self.x = self.initial_solution()
+
+    # Decoding
+    @staticmethod
+    def decode_position(p):
+        i = int(p / 10)
+        j = p % 10
+        return i, j
 
     # Getting initial solution using minimum-cost method
     # Trazenje pocetnog resenja metodom minimalnih cena
@@ -114,28 +123,27 @@ class TP:
             for w in adj_list[v]:
                 available = True
                 # Disable stepping over already selected basic variable.
-                wi = int(w / 10)
-                wj = w % 10
-                vi = int(v / 10)
-                vj = v % 10
+                wi, wj = TP.decode_position(w)
+                vi, vj = TP.decode_position(v)
+                row_num_basic = 0
+                col_num_basic = 0
                 for p in path:
+                    pi, pj = TP.decode_position(p)
+                    if pi == wi:
+                        row_num_basic += 1
+                    if pj == wj:
+                        col_num_basic += 1
                     if p != v and p != w:
-                        pi = int(p / 10)
-                        pj = p % 10
                         if ((vi == pi == wi and (vj < pj < wj or wj < pj < vj)) or
                            (vj == pj == wj and (wi < pi < vi or vi < pi < wi))) and w not in marked:
-                            # print("v")
-                            # print(v)
-                            # print("p")
-                            # print(p)
-                            # print("w")
-                            # print(w)
-                            # print()
                             available = False
+
+                if (row_num_basic > 1 or col_num_basic > 1) and w != start:
+                    available = False
+
                 if len(path) > 1:
                     p = path[-2]
-                    pi = int(p / 10)
-                    pj = p % 10
+                    pi, pj = TP.decode_position(p)
                     if ((vi == pi == wi and (vj < wj < pj or pj < wj < vj)) or
                        (vj == pj == wj and (pi < wi < vi or vi < wi < pi))) and w not in marked:
                         available = False
@@ -152,93 +160,144 @@ class TP:
         return []
 
     def method_of_potentials(self):
-        # Determining the row/column with maximum number of basic variables
-        i = 0
-        max_basic_i = 0
-        for k in range(self.x.shape[0]):
-            num_basic = np.count_nonzero(self.x[k] > 0)
-            if num_basic > max_basic_i:
-                max_basic_i = num_basic
-                i = k
-        j = 0
-        max_basic_j = 0
-        for k in range(self.x.shape[1]):
-            num_basic = np.count_nonzero(self.x[:, k] > 0)
-            if num_basic > max_basic_j:
-                max_basic_j = num_basic
-                j = k
+        print("\n\n***********************************")
+        print("Method of potentials, step by step")
+        print("***********************************")
+        iteration = 1
+        while True:
+            # Determining the row/column with maximum number of basic variables
+            i = 0
+            max_basic_i = 0
+            for k in range(self.x.shape[0]):
+                num_basic = np.count_nonzero(self.x[k] > 0)
+                if num_basic > max_basic_i:
+                    max_basic_i = num_basic
+                    i = k
+            j = 0
+            max_basic_j = 0
+            for k in range(self.x.shape[1]):
+                num_basic = np.count_nonzero(self.x[:, k] > 0)
+                if num_basic > max_basic_j:
+                    max_basic_j = num_basic
+                    j = k
 
-        u = np.full((1, len(self.a)), np.inf)[0]
-        v = np.full((1, len(self.b)), np.inf)[0]
+            u = np.full((1, len(self.a)), np.inf)[0]
+            v = np.full((1, len(self.b)), np.inf)[0]
 
-        # Setting that row/column of vector u/v to zero and calculating rest of u and v elements
-        if max_basic_i >= max_basic_j:
-            u[i] = 0
-        else:
-            v[j] = 0
+            # Setting that row/column of vector u/v to zero and calculating rest of u and v elements
+            if max_basic_i >= max_basic_j:
+                u[i] = 0
+            else:
+                v[j] = 0
 
-        while np.any(u == np.inf) or np.any(v == np.inf):
-            for i in range(len(u)):
-                if u[i] != np.inf:
-                    for j in range(len(v)):
-                        if self.x[i][j] != 0 and v[j] == np.inf:
-                            v[j] = self.c[i][j] - u[i]
-            for j in range(len(v)):
-                if v[j] != np.inf:
-                    for i in range(len(u)):
-                        if self.x[i][j] != 0 and u[i] == np.inf:
-                            u[i] = self.c[i][j] - v[j]
+            while np.any(u == np.inf) or np.any(v == np.inf):
+                # print(u)
+                # print(v)
+                for i in range(len(u)):
+                    if u[i] != np.inf:
+                        for j in range(len(v)):
+                            if self.x[i][j] != 0 and v[j] == np.inf:
+                                v[j] = self.c[i][j] - u[i]
+                for j in range(len(v)):
+                    if v[j] != np.inf:
+                        for i in range(len(u)):
+                            if self.x[i][j] != 0 and u[i] == np.inf:
+                                u[i] = self.c[i][j] - v[j]
 
-        # Finding smallest value less than zero: c(non-basic)_i,j - (u_i + v_j).
-        # If all greater than or equal zero, optimal solution is found
-        neg_values = []
-        for i in range(self.c.shape[0]):
-            for j in range(self.c.shape[1]):
-                if self.x[i][j] == 0:
-                    curr_diff = self.c[i][j] - (u[i] + v[j])
-                    if curr_diff < 0:
-                        neg_values.append((curr_diff, [i, j]))
+            # Finding smallest value less than zero: c(non-basic)_i,j - (u_i + v_j).
+            # If all greater than or equal zero, optimal solution is found
+            neg_values = []
+            for i in range(self.c.shape[0]):
+                for j in range(self.c.shape[1]):
+                    if self.x[i][j] == 0:
+                        curr_diff = self.c[i][j] - (u[i] + v[j])
+                        if curr_diff < 0:
+                            neg_values.append((curr_diff, [i, j]))
 
-        # Optimal solution found
-        if len(neg_values) == 0:
-            print("Optimal solution found!")
+            # Optimal solution found
+            if len(neg_values) == 0:
+                print("***********************************")
+                print("Optimal solution found after %s iterations!" % str(iteration - 1))
+                print("***********************************")
+                print("Matrix X containing basic variables - amount of merchandise being transported\n"
+                      "from i-th storage to j-th shop:")
+                print(self.x)
+                result = 0
+                for i in range(self.m):
+                    for j in range(self.n):
+                        if self.x[i][j] != 0:
+                            result += self.x[i][j] * self.c[i][j]
+                print("min = %s" % result)
+                break
+
+            # Positioning theta on the smallest negative value. Creating cycle - start from theta position, go
+            # through basic variables and return at the starting position.
+            theta = np.zeros(shape=(self.c.shape[0], self.c.shape[1]))
+            # print(sorted(neg_values))
+            pos_i = sorted(neg_values)[0][1][0]
+            pos_j = sorted(neg_values)[0][1][1]
+            self.x[pos_i][pos_j] = 1
+            adjacency_list = self.calculate_adjacency_list()
+            self.x[pos_i][pos_j] = 0
+
+            # print(pos_i, pos_j)
+
+            marked = {}
+            start = pos_i * 10 + pos_j
+            # print(adjacency_list)
+            print(start)
+            print(self.x)
+            positions = TP.find_theta_cycle(adjacency_list, marked, start)[:-1]
+            sign = -1
+            # Decode values and fill theta matrix
+            for pos in positions:
+                sign *= -1
+                i, j = TP.decode_position(pos)
+                theta[i][j] = sign
+            theta[pos_i][pos_j] = 1
+
+            indexes = np.nonzero(theta == -1)
+            theta_min = np.inf
+            print("positions")
+            print(positions)
+            # print(indexes)
+            for i in range(len(indexes[0])):
+                curr_theta = self.x[indexes[0][i]][indexes[1][i]]
+                if curr_theta < theta_min:
+                    theta_min = curr_theta
+
+            print("--------------")
+            print("Iteration %s" % iteration)
+            print("--------------")
+
+            full_matrix = np.c_[self.c, self.a]
+            print("Matrix C (transportation cost from i-th storage to j-th shop),\n"
+                  "vector A (amount of merchandise in i-th storage), on the right\n"
+                  "and vector B (demand of j-th shop), at the bottom: \n%s\n %s" % (full_matrix, self.b))
+            theta *= theta_min
+            print("Cycle starting from position (%i, %i): " % (pos_i, pos_j))
+            print(theta)
+            print("theta = %s" % theta_min)
+            self.x -= -theta
+            print("Matrix X containing basic variables - amount of merchandise being transported\n"
+                  "from i-th storage to j-th shop:")
             print(self.x)
 
-        # Positioning theta on the smallest negative value. Creating cycle - start from theta position, go
-        # through basic variables and return at the starting position.
-        theta = np.zeros(shape=(self.c.shape[0], self.c.shape[1]))
-        pos_i = sorted(neg_values)[0][1][0]
-        pos_j = sorted(neg_values)[0][1][1]
-        # theta[pos_i][pos_j] = 1
-        self.x[pos_i][pos_j] = 1
-        adjacency_list = self.calculate_adjacency_list()
-        self.x[pos_i][pos_j] = 0
-
-        print(adjacency_list)
-        marked = {}
-        start = pos_i * 10 + pos_j
-        positions = TP.find_theta_cycle(adjacency_list, marked, start)[:-1]
-        print(positions)
-        sign = -1
-        # Decode values and fill theta matrix
-        for pos in positions:
-            sign *= -1
-            i = int(pos / 10)
-            j = pos % 10
-            theta[i][j] = sign
-        theta[pos_i][pos_j] = 1
-        print(pos_i, pos_j)
-        # print(sorted(neg_values))
-        print(theta)
+            iteration += 1
 
     def solve_problem(self):
-        print("Initial solution found using minimum-cost method: \n%s" % self.x)
-        # print("a")
-        # print(self.a)
-        # print("b")
-        # print(self.b)
-        # print("c")
-        # print(self.c)
+        print("***********************************")
+        print("Input:")
+        print("***********************************")
+        full_matrix = np.c_[self.initial_c, self.initial_a]
+        print("Matrix C (transportation cost from i-th storage to j-th shop),\n"
+              "vector A (amount of merchandise in i-th storage), on the right\n"
+              "and vector B (demand of j-th shop), at the bottom: \n%s\n %s\n\n" % (full_matrix, self.b))
+        print("***********************************")
+        print("Initial solution found using minimum-cost method:")
+        print("***********************************")
+        print("Matrix X containing basic variables - amount of merchandise being transported\n"
+              "from i-th storage to j-th shop: \n%s" % self.x)
 
         self.method_of_potentials()
 
@@ -286,9 +345,9 @@ def main():
         a = np.array([28, 13, 19, 18])
         b = np.array([24, 16, 10, 20, 22])
         # matrix_c = np.array([[1, 0, 0, 0, 0], [4, 2, 0, 1, 4], [4, 5, 6, 2, 0], [0, 0, 0, 1, 0]])
-        # matrix_c = np.array([[1, 0, 5, 6, 9], [4, 2, 7, 9, 4], [14, 25, 16, 21, 5], [3, 4, 2, 6, 1]])
-        # matrix_c = np.array([[1, 2, 3, 4, 5], [14, 12, 10, 11, 24], [34, 15, 6, 2, 7], [8, 8, 9, 1, 2]])
-        matrix_c = np.array([[3, 9, 8, 10, 4], [6, 10, 3, 2, 3], [3, 2, 7, 10, 3], [3, 2, 3, 2, 8]])
+        # matrix_c = np.array([[1, 8, 5, 6, 9], [4, 2, 7, 9, 4], [14, 25, 16, 21, 5], [3, 4, 2, 6, 1]])
+        matrix_c = np.array([[1, 2, 3, 4, 5], [14, 12, 10, 11, 24], [34, 15, 6, 2, 7], [8, 8, 9, 1, 2]])
+        # matrix_c = np.array([[3, 9, 8, 10, 4], [6, 10, 3, 2, 3], [3, 2, 7, 10, 3], [3, 2, 3, 2, 8]])
 
         problem = TP(num_storages, num_shops, a, b, matrix_c)
         start_logging_to_file(output_file, mode)
